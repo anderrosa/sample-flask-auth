205724d19197a944db5882f659a27f968d2c924b
Anderson Rosa <andersilvars7@gmail.com>
2025-01-28 20:18:54 -0300

    Creating user delete routes

--- app.py
+++ app.py
@@ -74,8 +74,23 @@ def update_user(id_user):
    if user and data.get("password"):
       user.password = data.get("password")
       db.session.commit()
-      
+
       return jsonify({"message": f"Usuário {id_user} atualizado com sucesso"})
+   
+   return jsonify({"message": "Usuário não encontrado"}), 404
+
+@app.route('/user/<int:id_user>', methods=["DELETE"])
+@login_required
+def delete_user(id_user):
+   user = User.query.get(id_user)
+
+   if id_user == current_user.id:
+      return jsonify({"message": "Deleção não permitida"}), 403
+
+   if user:
+      db.session.delete(user)
+      db.session.commit()
+      return jsonify({"message": f"Usuário {id_user} deletado com sucesso"})
    
    return jsonify({"message": "Usuário não encontrado"}), 404
 
